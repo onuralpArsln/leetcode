943eb14ea9fd9ecacff2fde71388db262a23bc6e
hackerFaruk <onuralparslan97@gmail.com>
2023-04-02 14:56:01 +0300

    docs

--- zigzagconv.py
+++ zigzagconv.py
@@ -1,3 +1,5 @@
+# For each row, we can get the characters in that row by starting at the first character of the string
+# and incrementing by the number of characters between two adjacent characters in that row.
 class Solution(object):
     def convert(self, s, numRows):
         """
@@ -11,6 +13,20 @@ class Solution(object):
         res =''
         arrCount = 2*numRows-2
 
+# Creating an array of strings, each string is a row of the zigzag pattern.
+#         For example, if numRows = 4, then resArr = ['PAYPALISHIRING', 'APLSIIG', 'YIR', 'P']
+#         The first string is the first row of the zigzag pattern, the second string is the second row
+# of the zigzag pattern, etc.
+#         The first string is created by starting at the first character of the string and
+# incrementing by the number of characters between two adjacent characters in that row.
+#         The second string is created by starting at the second character of the string and
+# incrementing by the number of characters between two adjacent characters in that row.
+#         The third string is created by starting at the third character of the string and
+# incrementing by the number of characters between two adjacent characters in that row.
+#         The fourth string is created by starting at the fourth character of the string and
+# incrementing by the number of characters between two adjacent characters in that row.
+#         The fifth string is created by starting at the fifth character of the string and
+# incrementing
 
         for i in range(2*numRows-2):
             resArr.append(word[i::2*numRows-2])
