32b213e38d20703691fab741bfef3ef8077e86d0
Onuralp <58907470+onuralpArsln@users.noreply.github.com>
2024-11-23 00:09:45 +0300

    Time: 4 ms (96.60%) | Memory: 45.6 MB (11.21%) - LeetSync

---
+++ 155-min-stack/min-stack.java
@@ -0,0 +1,45 @@
+class MinStack {
+    private int[] realStack;
+    private int realPointer;
+    private int[] minValueStack;
+    private int minValuePointer;
+
+    public MinStack() {
+        realStack = new int[25600];
+        realPointer = -1;
+        minValueStack = new int[25600];
+        minValuePointer = -1;
+    }
+
+    public void push(int val) {
+        realPointer++;
+        realStack[realPointer] = val;
+
+        if (minValuePointer == -1 || val <= minValueStack[minValuePointer]) {
+            minValuePointer++;
+            minValueStack[minValuePointer] = val;
+        }
+    }
+
+    public void pop() {
+        if (realPointer >= 0) {
+            if (realStack[realPointer] == minValueStack[minValuePointer]) {
+                minValuePointer--;
+            }
+            realPointer--;
+        }
+    }
+
+    public int top() {
+        if (realPointer < 0) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return realStack[realPointer];
+    }
+
+    public int getMin() {
+        if (minValuePointer < 0) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return minValueStack[minValuePointer];}
+}
